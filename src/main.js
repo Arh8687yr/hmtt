0ee32251cb452f1ea3e889b0fef21c0c79a6fabc
执念 <1250696730@qq.com>
2019-08-21 20:47:38 +0800

    设置全局前置导航守卫

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './premission'
 import ElementUI from 'element-ui' // 引入Element-UI
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './assets/style/index.less' // 引入less样式文件
@@ -10,7 +10,8 @@ import Component from './components' // 引入自定义全局组件
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 将后台接口的地址给axios的baseURL
 
 Vue.prototype.$axios = axios // 将axios给Vue原型
-Vue.use(Component)
+Vue.use(Component) // 使用自定义全局组件
+
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
