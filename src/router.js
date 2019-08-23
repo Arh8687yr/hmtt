148cbecf155d3dab5567089af8d1a52b260c6cb4
执念 <1250696730@qq.com>
2019-08-23 21:49:47 +0800

    评论列表分页完成

--- src/router.js
+++ src/router.js
@@ -35,13 +35,6 @@ export default new Router({
           component: () => import('./views/articles') // 按需加载的写法
         }
       ]
-      // path: '/about',
-      // name: 'about',
-      // // route level code-splitting
-      // // this generates a separate chunk (about.[hash].js) for this route
-      // // which is lazy-loaded when the route is visited.
-      // component: () =>
-      //             import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -18,11 +18,24 @@
       </el-table-column>
       <el-table-column label="开启/禁止评论">
         <template slot-scope="obj">
-          <el-button @click="closeOpen(obj.row)" :style="{color:obj.row.comment_status?'#E6A23C':'#409'}" type="text">{{obj.row.comment_status?"禁止评论":"开启评论"}}</el-button>
+          <el-button
+            @click="closeOpen(obj.row)"
+            :style="{color:obj.row.comment_status?'rgb(230, 162, 60)':'rgb(103, 194, 58)'}"
+            type="text"
+          >{{obj.row.comment_status?"禁止评论":"开启评论"}}</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination @current-change="changePage" background layout="prev, pager, next" :total="1000"></el-pagination>
+    <el-row type="flex" justify="center" style="margin:20px 0">
+      <el-pagination
+        @current-change="changePage"
+        background
+        layout="prev, pager, next"
+        :page-size="page.pageSize"
+        :total="page.total"
+        :current-page="page.currentPage"
+      ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -31,30 +44,30 @@ export default {
   data () {
     return {
       //  定义表格数据
-      commentList: []
+      commentList: [],
+      page: {
+        // pageSize: 每页显示多少条数据
+        // total: 总共多少条数据
+        // currentPage: 当前显示第几页数据
+        pageSize: 10,
+        total: 0,
+        currentPage: 1
+      }
     }
   },
   methods: {
-    getList () {
-      this.$axios({
-        url: '/articles',
-        params: {
-          response_type: 'comment' // 查询评论相关的数据
-        }
-      }).then(result => {
-        this.commentList = result.data.results
-      })
+
+    changePage (newPage) {
+    // current-change是分页组件的事件，在页码改变时触发，会传过来当前点击的页码值
+
+      // 将当前最新页码赋值给currentPage
+      this.page.currentPage = newPage
+      // 重新加载数据
+      this.getList()
     },
-    formatter (row, column, cellValue, index) {
-      // element-ui提供的方法，用来格式化内容（因为不支持将布尔值的数据显示到页面）
-      // 因为需要接收返回值，所以必须返回
-      // row: 当条数据对象
-      // column: 当前列属性
-      // cellVaule: 当前单元格属性值
-      return cellValue ? '开启' : '禁止'
-    },
+
     closeOpen (row) {
-      // 设置评论状态是否开启
+    // 设置评论状态是否开启
       let mess = row.comment_status ? '禁止' : '开启'
       this.$confirm(`确定要${mess}评论吗？`, '提示').then(() => {
         this.$axios({
@@ -67,8 +80,34 @@ export default {
         })
       })
     },
-    changePage (newPage) {
-      // current-change是分页组件的事件，在页码改变时触发，会传过来当前点击的页码值
+
+    formatter (row, column, cellValue, index) {
+    // 处理大数字不能正常获取的问题
+
+      // element-ui提供的方法，用来格式化内容（因为不支持将布尔值的数据显示到页面）
+      // 因为需要接收返回值，所以必须返回
+      // row: 当条数据对象
+      // column: 当前列属性
+      // cellVaule: 当前单元格属性值
+      return cellValue ? '开启' : '禁止'
+    },
+
+    getList () {
+      // pageList 保存获取到的当前页和每页显示的数据
+      let pageList = {
+        page: this.page.currentPage, per_page: this.page.pageSize
+      }
+      this.$axios({
+        url: '/articles',
+        params: {
+          response_type: 'comment', // 查询评论相关的数据
+          ...pageList
+        }
+      }).then(result => {
+        this.commentList = result.data.results
+        // 获取文章的总条数
+        this.page.total = result.data.total_count
+      })
     }
   },
   created () {
