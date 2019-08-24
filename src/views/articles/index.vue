8a7825b39edf3282de08a98bcb3ce436e88be4db
执念 <1250696730@qq.com>
2019-08-24 15:15:35 +0800

    获取内容列表及分页

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -15,14 +15,20 @@
         <el-form-item>
           <template>
             <span>频道列表：</span>
-            <el-select></el-select>
+            <el-select v-model="value" placeholder="请选择">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              ></el-option>
+            </el-select>
           </template>
         </el-form-item>
         <el-form-item>
           <template>
             <span>时间选择：</span>
             <el-date-picker
-              v-model="value9"
               type="daterange"
               start-placeholder="开始日期"
               end-placeholder="结束日期"
@@ -38,12 +44,12 @@
         <div class="list" v-for="(item,index) in lists" :key="index">
           <div class="left">
             <div class="bgimg">
-              <img src="../../assets/img/404.png" alt />
+              <img :src="item.cover.images[0] ? item.cover.images[0]:baseURL" alt />
             </div>
             <div class="info">
-              <p>123456789</p>
-              <span>状态</span>
-              <p>2019年8月24日10:34:19</p>
+              <p>{{item.title}}</p>
+              <span>{{item.status}}</span>
+              <p>{{item.pubdate}}</p>
             </div>
           </div>
           <div class="right">
@@ -62,9 +68,9 @@
         @current-change="changePage"
         background
         layout="prev, pager, next"
-        :page-size="10"
-        :total="100"
-        :current-page="1"
+        :page-size="page.pageSize"
+        :total="page.total"
+        :current-page="page.currentPage"
       ></el-pagination>
     </el-row>
   </el-card>
@@ -75,8 +81,65 @@ export default {
   data () {
     return {
       radio: '1',
-      lists: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      lists: [],
+      baseURL: require('../../assets/img/404.png'), // require将默认地址转为base64
+      // 定义无图时默认现实的图片
+      page: {
+        pageSize: 10,
+        total: 0,
+        currentPage: 1
+      },
+      options: [
+        {
+          value: '选项1',
+          label: '黄金糕'
+        },
+        {
+          value: '选项2',
+          label: '双皮奶'
+        },
+        {
+          value: '选项3',
+          label: '蚵仔煎'
+        },
+        {
+          value: '选项4',
+          label: '龙须面'
+        },
+        {
+          value: '选项5',
+          label: '北京烤鸭'
+        }
+      ],
+      value: ''
     }
+  },
+  methods: {
+    changePage (newPage) {
+      // 获取当前点击页的内容
+      this.page.currentPage = newPage
+      this.getList()
+    },
+    getList () {
+      // 将文章当前页与文章每页显示的条数作为对象 打散后拼接到url上
+      let Params = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize
+      }
+      // 请求文章内容
+      this.$axios({
+        url: '/articles',
+        params: {
+          ...Params
+        }
+      }).then(result => {
+        this.lists = result.data.results // 获取文章列表
+        this.page.total = result.data.total_count // 获取文章总条数
+      })
+    }
+  },
+  created () {
+    this.getList()
   }
 }
 </script>
@@ -110,7 +173,6 @@ export default {
           .bgimg {
             width: 180px;
             height: 130px;
-            background-color: red;
             margin-right: 15px;
             img {
               width: 100%;
